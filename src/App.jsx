a493a915b6f7a0351e9eea19f0b0d1b259cbbc1e
PatricioIgnacio <patricioignacio.santibanezgonz@gmail.com>
2023-09-01 01:57:11 -0400

    Creación de Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
+import { useFetch } from "./useFetch";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { data } = useFetch("https://jsonplaceholder.typicode.com/users/");
 
   return (
     <>
-      <div>
+      <div className="App">
         <h1>Llamado a la API</h1>
+        <div className="card">
+          <ul>
+            {data?.map((user) => (
+              <li key={user.id}>{user.name}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
